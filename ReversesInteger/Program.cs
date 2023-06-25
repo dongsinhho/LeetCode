a368128491b011555f91faa143a97b27e67160c9
dongsinhho <sinhvua@gmail.com>
2023-06-25 13:19:30 +0700

    ReverseInterger

---
+++ ReversesInteger/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int result = Reverse(120);
+        Console.WriteLine(result);
+    }
+
+    public static int Reverse(int x)
+    {
+        string strResult = "";
+        foreach (var item in x.ToString())
+        {
+            strResult = item + strResult;
+        }
+        if (x < 0)
+        {
+            strResult = strResult.Substring(0, strResult.Length - 1);
+            var result = long.Parse(strResult);
+            if (-result < int.MinValue)
+            {
+                return 0;
+            }
+            return -(int)result;
+        }
+        else
+        {
+            var result = long.Parse(strResult);
+            if (result > int.MaxValue)
+            {
+                return 0;
+            }
+            return (int)result;
+        }
+    }
+}
+
+
